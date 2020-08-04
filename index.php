3c6e038e0dcd56a256cf793cd5b20b5b0ca91714
LittleGyaani <bmohanty@live.com>
2020-08-04 08:18:04 +0530

    Fixed Google Map

--- index.php
+++ index.php
@@ -748,7 +748,7 @@
                     <div class="col-12">
                         <div class="google-map">
                             <div class="embed-responsive embed-responsive-21by9">
-                                <iframe class="embed-responsive-item" src="https://www.google.com/maps/place/India/@20.7707739,73.7217954,5z/data=!3m1!4b1!4m5!3m4!1s0x30635ff06b92b791:0xd78c4fa1854213a6!8m2!3d20.593684!4d78.96288" allowfullscreen="yes"></iframe>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15280155.967092693!2d73.721795421602!3d20.770773939758556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1596509219808!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                             </div>
                         </div>
                         <br/>
